395f7b40f697d233b242e299e0eb2f527e3213d8
Johan Svensson <github.js@crypt.se>
2016-09-01 16:09:26 +0200

    Dry code: Messed around with some logics

--- plox/plox.ino
+++ plox/plox.ino
@@ -99,6 +99,7 @@ boolean read_controllers()
 
 boolean send_baseunit()
 {
+    return true;
 }
 
 void loop()
@@ -126,32 +127,34 @@ void loop()
     }
     else 
     {
+        if(!digitalRead(CAN0_INT))                         // If CAN0_INT pin is low, read receive buffer
+        {
+            CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+
+            if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
+                sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+            else
+                sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
+
+            Serial.print(msgString);
+
+            if((rxId & 0x40000000) == 0x40000000){    // Determine if message is a remote request frame.
+                sprintf(msgString, " REMOTE REQUEST FRAME");
+                Serial.print(msgString);
+            } else {
+                for(byte i = 0; i<len; i++){
+                    sprintf(msgString, " 0x%.2X", rxBuf[i]);
+                    Serial.print(msgString);
+                }
+            }
+
+            Serial.println();
+        }
+
         if (send_baseunit())
         {
-            if(!digitalRead(CAN0_INT))                         // If CAN0_INT pin is low, read receive buffer
-            {
-                CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
-
-                if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
-                    sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
-                else
-                    sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
-
-                Serial.print(msgString);
-
-                if((rxId & 0x40000000) == 0x40000000){    // Determine if message is a remote request frame.
-                    sprintf(msgString, " REMOTE REQUEST FRAME");
-                    Serial.print(msgString);
-                } else {
-                    for(byte i = 0; i<len; i++){
-                        sprintf(msgString, " 0x%.2X", rxBuf[i]);
-                        Serial.print(msgString);
-                    }
-                }
-
-                Serial.println();
-            }
         }
+
     }
 }
 
