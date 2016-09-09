9d456c290d1eff3c960d04caa454aa1b883f48f6
Johan Svensson <github.js@crypt.se>
2016-09-09 23:47:03 +0200

    Generalizing connection names, starting the work to support two players

--- baseunit/nes/nes.ino
+++ baseunit/nes/nes.ino
@@ -8,11 +8,24 @@
 
 #define CTR_CLOCK  0b00001000   // Controller clock pin
 #define CTR_LATCH  0b00010000   // Controller latch pin
-#define CTR_DATA_0 0b01000000  // Controller 0 data pin
+#define CTR_DATA_0 0b00100000  // Controller 0 data pin
 #define CTR_DATA_1 0b01000000  // Controller 0 data pin
 
-#define CTR_DATA_2 7  // Controller 2 data pin
-#define CTR_DATA_3 8  // Controller 3 data pin
+#define CLK_I PIND
+#define CLK_B 0x08
+#define LTC_I PIND
+#define LTC_B 0x10
+#define DA0_O PORTD
+#define DA0_B 0x20
+#define DA1_O PORTB
+#define DA1_B 0x01
+
+#define setHigh(myPort, myBit) myPort |= myBit
+#define setLow(myPort, myBit) myPort &= ~myBit
+#define setBit(myPort, myBit, myVal) if (myVal) myPort |= myBit; else myPort &= ~myBit
+
+#define toggle(myPort, myBit) myPort ^= myBit
+#define readBit(myPort, myBit) ((myPort & myBit) != 0)
 
 #define JOYS 2        // Number of joysticks/controllers
 #define CLOCKS 8     // Number of pulses to send to controllers
@@ -40,33 +53,41 @@ void setup()
   CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
   pinMode(CAN0_INT, INPUT);   // Configuring pin for /INT input
 
-  DDRD |= 0b11100000;
-  PORTD |= 0b11100000;
+  DDRD |= DA0_B;
+  DDRB |= DA1_B;
+  setHigh(DA0_O, DA0_B);
+  setHigh(DA1_O, DA1_B);
   noInterrupts();
 }
 
 boolean send_baseunit()
 {
   unsigned int joy_data[JOYS];
-  byte joy_pin[4] = {CTR_DATA_0, CTR_DATA_1, CTR_DATA_2, CTR_DATA_3};
+  byte *joy_o[JOYS] = {&DA0_O, &DA1_O};
+  byte joy_b[JOYS] = {DA0_B, DA1_B};
 
-  if (!(PIND & CTR_LATCH))
+  //if (!(PIND & CTR_LATCH))
+    //return false;
+  if (!readBit(LTC_I, LTC_B))
     return false;
+
+  setBit(*joy_o[1], joy_b[1], 0);
+  delayMicroseconds(3);
+  setBit(DA1_O, DA1_B, 1);
 
   joy_data[0] = g_joy_data[0];
 
-  if (joy_data[0] & 0x8000) PORTD &= ~CTR_DATA_0; // write first data bit
-  else PORTD |= CTR_DATA_0;
-  while (PIND & CTR_LATCH); // wait for latch to finish
-  for (int i = 0; i < CLOCKS; i++) {
-    while (!(PIND & CTR_CLOCK)); // wait for clock low
-    //delayMicroseconds(3);
-    joy_data[0] <<= 1; // shift data
-    if (joy_data[0] & 0x8000) PORTD &= ~CTR_DATA_0;
-    else PORTD |= CTR_DATA_0;
-    while (PIND & CTR_CLOCK); // wait for clock high
+  for (byte j = 0; j < 1; j++) {
+    setBit(*joy_o[j], joy_b[j], !(joy_data[j] & 0x8000));
+    while (readBit(LTC_I, LTC_B)); // wait for latch to finish
+    for (int i = 0; i < CLOCKS; i++) {
+      while (!readBit(CLK_I, CLK_B)); // wait for clock low
+      joy_data[j] <<= 1; // shift data
+      setBit(*joy_o[j], joy_b[j], !(joy_data[j] & 0x8000));
+      while (readBit(PIND, CTR_CLOCK)); // wait for clock high
+    }
+    PORTD |= CTR_DATA_0;
   }
-  PORTD |= CTR_DATA_0;
   return true;
 }
 
@@ -97,12 +118,12 @@ void loop()
         }
         g_joy_data[i] = joy_data;
         //g_joy_data[i] = joy_data >> 8 | (joy_data << 8);
-        //sprintf(msgString, "joy_data[%d]: 0x%x", i, g_joy_data[i]);
-        //Serial.println(msgString);
+        sprintf(msgString, "joy_data[%d]: 0x%x", i, g_joy_data[i]);
+        Serial.println(msgString);
       }
     }
 
-    //Serial.println();
+    Serial.println();
   }
   //*/
 
